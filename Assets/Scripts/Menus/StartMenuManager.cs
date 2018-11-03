e0f4f5e030477980b2590cc5d08668cd98ac0361
Cláudia Marinho <claudia.m.r.marinho@gmail.com>
2018-11-03 22:41:01 +0000

    More UI improvements

--- Assets/Scripts/Menus/StartMenuManager.cs
+++ Assets/Scripts/Menus/StartMenuManager.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartMenuManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // default menu
+    public GameObject defaultMenu;
+
+    // high scores screen
+    public GameObject highScores;
+
+    // difficultymenu
+    public GameObject difficultyMenu;
+
+    // button corresponding to normal difficulty
+    public Button normalBtn;
+
+    // button corresponding to hard difficulty
+    public Button hardBtn;
+
+
 }
